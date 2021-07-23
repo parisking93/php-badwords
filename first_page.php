73c2bfd65e374333a1a39b07e9cb5a9e09a4be7f
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-07-23 15:21:32 +0200

    add testo e lunghezza testo

--- first_page.php
+++ first_page.php
@@ -1,5 +1,5 @@
 <!-- Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza. Una parola da censurare viene passata dall'utente tramite parametro GET. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
-<?php $testo = "ciao" ?>
+<?php $testo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, exercitationem." ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +9,8 @@
     <title>Document</title>
 </head>
 <body>
-    <h2><?php echo $testo ?></h2>
+    <h2><?php echo $testo?></h2>
+   
+    <h3><?php echo 'il testo è lungo   ' . strlen($testo) . ' caratteri'?></h3>
 </body>
 </html>
